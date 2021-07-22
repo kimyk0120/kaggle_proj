37fd6ca497bbedf1c09494451c2fc602435611e4
kimyk0120 <kimyk0120@hotmail.com>
2021-07-22 13:10:21 +0900

    no message

---
+++ exam/tf_ex_2_deepdream.py
@@ -0,0 +1,25 @@
+"""
+    Deep dream
+"""
+
+import tensorflow as tf
+import numpy as np
+import PIL.Image
+import time
+import functools
+import IPython.display as display
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+import tensorflow_hub as hub
+
+from tensorflow.keras.preprocessing import image
+
+print("prcs start")
+
+
+
+print("prcs fin")
+
+if __name__ == "__main__":
+    pass
